702caa72028536e699615d4e99372bef1d2a9ce4
AbelW <unoctium1@gmail.com>
2019-03-02 20:56:36 -0800

    Test compile, defined pins

--- src/CogsRobot.ino
+++ src/CogsRobot.ino
@@ -1,9 +1,25 @@
 /*
  * Project CogsRobot
- * Description:
- * Author:
- * Date:
+ * Description: Driver code for COGS/EML Workshop
+ * Author: Abel Waller
+ * Date: March 2, 2019
  */
+#include <HC_SR04/HC_SR04.h>
+
+
+#define MOTOR_A_1A 0
+#define MOTOR_B_1A 1
+#define MOTOR_A_1B 2
+#define MOTOR_B_1B 3
+
+#define LEFT_ECHO 4
+#define LEFT_TRIG 5
+
+#define RIGHT_ECHO 6
+#define RIGHT_TRIG 7
+
+HC_SR04 leftRange = HC_SR04(LEFT_TRIG, LEFT_ECHO);
+HC_SR04 rightRange = HC_SR04(RIGHT_TRIG, RIGHT_ECHO);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
